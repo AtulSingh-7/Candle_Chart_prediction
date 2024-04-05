b0a6e77d9028e832d2f51a29dc6ba692f8d58196
AtulSingh-7 <105373519+AtulSingh-7@users.noreply.github.com>
2024-04-05 18:58:21 +0530

    Add files via upload
    Signed-off-by: AtulSingh-7 <105373519+AtulSingh-7@users.noreply.github.com>

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Title from './components/Title.jsx';
+import LeftLive from './components/LeftLive.jsx';
+import RightPredicted from './components/RightPredicted.jsx';
+
+function App() {
+  return (
+    <div>
+    <Title />
+    <LeftLive />
+    <RightPredicted />
+    </div>
+    
+  );
+}
+
+export default App;
